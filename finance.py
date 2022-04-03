bcf133ae130c113470bf7b1abbf6d877070b2600
Aakif Akhtar <san.akhtar7787@gmail.com>
2022-04-03 11:53:46 +0530

    commit changes

--- finance.py
+++ finance.py
@@ -1,3 +1,4 @@
+from cmath import phase
 from time import time
 import matplotlib.pyplot as plt
 import pennylane as qml
@@ -15,5 +16,19 @@ class Finance:
         self.cost_history = []
 
 
-    def F_phase_estimation(self):
-        pass
+    def single_qubit_layer(self, phase):
+        for i in range(self.total_qubit):
+            qml.RX(phase, i)
+            qml.RZ(phase, i)
+
+    def entangling_layer(self):
+        for i in range(self.total_qubit):
+            if i!=self.total_qubit-1:
+
+                qml.IsingXX(wires=(i, i+1))
+                qml.IsingXX(wires=(i, i+2))
+                qml.IsingXX(wires=(i, i+3))
+
+    def quantum_circuit_born_machine(self, phase):
+        self.single_qubit_layer(phase)
+        self.entangling_layer()
